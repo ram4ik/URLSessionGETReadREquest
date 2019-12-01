94c3ab16080d0df6b293a2845b340891d5d947d0
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2019-12-01 21:05:48 +0200

    REST with id

--- URLSessionGETReadREquest/ContentView.swift
+++ URLSessionGETReadREquest/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Button(action: {
-                Music.fetch()
+                Music.fetch(1)
             }) {
                 Text("Fetch data")
             }
--- URLSessionGETReadREquest/Music.swift
+++ URLSessionGETReadREquest/Music.swift
@@ -8,10 +8,12 @@
 
 import Foundation
 
+let myUrl = "https://www.orangevalleycaa.org/api/"
+
 class Music {
     
-    static func fetch() {
-        let urlString = "https://www.google.com/"
+    static func fetch(_ id: Int) {
+        let urlString = myUrl + "music/id/\(id)"
         
         if let url = URL.init(string: urlString) {
             let task = URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
